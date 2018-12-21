fd633a99aa116c7c9a3ed6f8875aa1d9b1f2b0e0
jun <leebduk@gmail.com>
2018-12-21 19:14:20 +0900

    Add ast.go

---
+++ ast/ast.go
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2018 De-labtory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package ast
+
+// Represent Statement
+type Statement interface {
+}
+
+// Represent Expression
+type Expression interface {
+}
+
+// Represent Program.
+// Program consists of multiple statements.
+type Program struct {
+	Statements []Statement
+}
