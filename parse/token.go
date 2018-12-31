eef4a4bd8cad184e7b11cb8df1c30bde50020df6
zerofruit <14wnrkim@gmail.com>
2018-12-31 22:20:24 +0900

    define TokenTypeMap

--- parse/token.go
+++ parse/token.go
@@ -70,3 +70,46 @@ const (
 	Else   // else
 	Return // return
 )
+
+// TokenTypeMap mapping TokenType with its
+// string, this helps debugging
+var TokenTypeMap = map[TokenType]string{
+	Illegal: "ILLEGAL",
+
+	Ident:  "IDENT",
+	Int:    "INT",
+	String: "STRING",
+	Bool:   "BOOL",
+
+	IntType:    "INT_TYPE",
+	StringType: "STRING_TYPE",
+	BoolType:   "BOOL_TYPE",
+
+	Assign:   "ASSIGN",
+	Plus:     "PLUS",
+	Minus:    "MINUS",
+	Bang:     "BANG",
+	Asterisk: "ASTERISK",
+	Slash:    "SLASH",
+	Mod:      "MOD",
+
+	LT:     "LT",
+	GT:     "GT",
+	LTE:    "LTE",
+	GTE:    "GTE",
+	EQ:     "EQ",
+	NOT_EQ: "NOT_EQ",
+
+	Comma: "COMMA",
+
+	Lparen: "LPAREN",
+	Rparen: "RPAREN",
+	Lbrace: "LBRACE",
+	Rbrace: "RBRACE",
+
+	True:   "TRUE",
+	False:  "FALSE",
+	If:     "IF",
+	Else:   "ELSE",
+	Return: "RETURN",
+}
