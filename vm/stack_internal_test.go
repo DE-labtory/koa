c3c7f56894423e68aa2d2896ddaa31441dcaa12a
KIMSJ <leesd556@gmail.com>
2019-01-01 23:48:04 +0900

    Add stack's testcase

--- vm/stack_internal_test.go
+++ vm/stack_internal_test.go
@@ -16,24 +16,80 @@
 
 package vm
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestStack_new(t *testing.T) {
+	stack := newStack()
+	if cap(stack.items) != stackMaxSize {
+		t.Fatalf("Stack initializing failed")
+	}
+}
 
+func TestStack_pushN(t *testing.T) {
+	stack := newStack()
+
+	stack.pushN(item(1), item(2), item(3))
+
+	if len(stack.items) != 3 {
+		t.Fatalf("PushN did not work properly")
+	}
 }
 
 func TestStack_push(t *testing.T) {
+	stack := newStack()
 
+	stack.push(item(1))
+	stack.push(item(2))
+	stack.push(item(3))
+	if len(stack.items) != 3 {
+		t.Fatalf("Push did not work properly")
+	}
 }
 
 func TestStack_pop(t *testing.T) {
+	stack := newStack()
 
+	stack.push(item(1))
+	stack.push(item(2))
+	stack.push(item(3))
+
+	for i := len(stack.items); i > 0; i-- {
+		if stack.pop() != item(i) {
+			t.Fatalf("Pop error at stack point %d", i)
+		}
+	}
+
+	if len(stack.items) != 0 {
+		t.Fatal("Stack has some datas")
+	}
 }
 
 func TestStack_len(t *testing.T) {
+	stack := newStack()
 
+	stack.push(item(1))
+	stack.push(item(2))
+	stack.push(item(3))
+
+	if len(stack.items) != stack.len() {
+		t.Fatal("function len is invalid")
+	}
 }
 
 func TestStack_print(t *testing.T) {
+	stack := newStack()
 
+	stack.push(item(1))
+	stack.push(item(2))
+	stack.push(item(3))
+
+	//	### stack ###
+	//	0    1
+	//	1    2
+	//	2    3
+	//	#############
+
+	stack.print()
 }
