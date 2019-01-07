2f7e5d8897a786acad8a983a97cddc320575ead7
성락원 <rakwonseong@seonglag-won-ui-MacBook-Pro.local>
2019-01-07 23:53:39 +0900

    implement encodingString & write down test case

--- encoding/encode_internal_test.go
+++ encoding/encode_internal_test.go
@@ -53,3 +53,40 @@ func Test_encodeInt(t *testing.T) {
 		}
 	}
 }
+
+func Test_encodeString(t *testing.T) {
+	tests := []struct {
+		operand  string
+		expected []byte
+	}{
+		{
+			operand:  "abc",
+			expected: []byte{0x61, 0x62, 0x63},
+		},
+		{
+			operand:  "1234567890",
+			expected: []byte{0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30},
+		},
+		{
+			operand:  "~!@#$%^&*()_+",
+			expected: []byte{0x7e, 0x21, 0x40, 0x23, 0x24, 0x25, 0x5e, 0x26, 0x2a, 0x28, 0x29, 0x5f, 0x2b},
+		},
+		{
+			operand:  "12!@qw",
+			expected: []byte{0x31, 0x32, 0x21, 0x40, 0x71, 0x77},
+		},
+	}
+
+	for i, test := range tests {
+		op := test.operand
+		bytecode, err := encodeString(op)
+
+		if err != nil {
+			t.Fatalf("test[%d] - encodeInt() had error. err=%v", i, err)
+		}
+
+		if !bytes.Equal(bytecode, test.expected) {
+			t.Errorf("test[%d] - encodeInt() result wrong. expected=%x, got=%x", i, test.expected, bytecode)
+		}
+	}
+}
