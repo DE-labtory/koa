951a75bca4b3d779db3b1cbe63e7ddbc8b8881f7
KIMSJ <leesd556@gmail.com>
2019-01-11 22:23:21 +0900

    Implement memory with testcase

---
+++ vm/memory.go
@@ -0,0 +1,150 @@
+// Copyright 2015 The go-ethereum Authors
+// This file is part of the go-ethereum library.
+//
+// The go-ethereum library is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// The go-ethereum library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with the go-ethereum library. If not, see <http://www.gnu.org/licenses/>.
+
+/*
+ * Copyright 2018 De-labtory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vm
+
+import (
+	"fmt"
+	"math/big"
+
+	"errors"
+
+	"github.com/ethereum/go-ethereum/common/math"
+)
+
+var ErrInvalidMemory = errors.New("Invalid memory reference")
+
+type Memory struct {
+	data []byte
+	cost uint32
+}
+
+func NewMemory() *Memory {
+	return &Memory{
+		data: make([]byte, 0),
+		cost: 0,
+	}
+}
+
+// Set sets offset to value
+func (m *Memory) Set(offset uint32, value byte) {
+	if offset > uint32(m.Len()) {
+		panic(ErrInvalidMemory)
+	}
+	m.data[offset] = value
+}
+
+// Set4 sets the 4 bytes starting at offset to the value of val, left-padded with zeroes to
+// 4 bytes.
+func (m *Memory) Set4(offset uint32, value []byte) {
+	tmp := new(big.Int)
+	tmp.SetBytes(value)
+
+	if offset+4 > uint32(m.Len()) {
+		panic(ErrInvalidMemory)
+	}
+	copy(m.data[offset:offset+4], []byte{0, 0, 0, 0})
+	math.ReadBits(tmp, m.data[offset:offset+4])
+}
+
+// Sets sets offset + size to value
+func (m *Memory) Sets(offset, size uint32, value []byte) {
+	if size > 0 {
+		if offset+size > uint32(m.Len()) {
+			panic(ErrInvalidMemory)
+		}
+		copy(m.data[offset:offset+size], value)
+	}
+}
+
+// Get returns offset + size as a new slice
+func (m *Memory) GetVal(offset, size uint32) []byte {
+	if size == 0 {
+		return nil
+	}
+
+	if uint32(m.Len()) > offset {
+		cpy := make([]byte, size)
+		copy(cpy, m.data[offset:offset+size])
+
+		return cpy
+	}
+
+	return nil
+}
+
+// GetPtr returns the offset + size
+func (m *Memory) GetPtr(offset, size uint32) []byte {
+	if size == 0 {
+		return nil
+	}
+
+	if len(m.data) > int(offset) {
+		return m.data[offset : offset+size]
+	}
+
+	return nil
+}
+
+// Resize resizes the memory to size
+func (m *Memory) Resize(size uint32) {
+	if uint32(m.Len()) < size {
+		m.data = append(m.data, make([]byte, size-uint32(m.Len()))...)
+	}
+}
+
+func (m *Memory) Len() int {
+	return len(m.data)
+}
+
+func (m *Memory) Data() []byte {
+	return m.data
+}
+
+func (m *Memory) Cost() uint32 {
+	return m.cost
+}
+
+// Print dumps the content of the memory.
+func (m *Memory) Print() {
+	fmt.Printf("### mem %d bytes ###\n", len(m.data))
+	if len(m.data) > 0 {
+		addr := 0
+		for i := 0; i+4 <= len(m.data); i += 4 {
+			fmt.Printf("%03d: % x\n", addr, m.data[i:i+4])
+			addr++
+		}
+	} else {
+		fmt.Println("-- empty --")
+	}
+	fmt.Println("####################")
+}
---
+++ vm/memory_test.go
@@ -0,0 +1,309 @@
+/*
+ * Copyright 2018 De-labtory
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package vm
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestMemory_New(t *testing.T) {
+
+}
+
+func TestMemory_Set(t *testing.T) {
+	memory := NewMemory()
+
+	tests := []struct {
+		offset uint32
+		data   []byte
+	}{
+		{0x00, []byte{0x00}},
+		{0x01, []byte{0x01}},
+		{0x02, []byte{0x02}},
+		{0x03, []byte{0x03}},
+	}
+
+	testsExpected := []struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		{0x00, []byte{0x00}, 1},
+		{0x01, []byte{0x01}, 1},
+		{0x02, []byte{0x02}, 1},
+		{0x03, []byte{0x03}, 1},
+	}
+
+	memory.Resize(4)
+
+	for _, test := range tests {
+		memory.Set(test.offset, test.data[0])
+	}
+
+	for _, expected := range testsExpected {
+		data := memory.GetVal(expected.offset, expected.size)
+
+		if !bytes.Equal(expected.data, data) {
+			t.Error("Invalid memory value")
+		}
+	}
+}
+
+func TestMemory_Set4(t *testing.T) {
+	memory := NewMemory()
+
+	tests := []struct {
+		offset uint32
+		data   []byte
+	}{
+		{0x00, []byte{0x00, 0x01, 0x02, 0x03}},
+		{0x04, []byte{0x00, 0x01, 0x02, 0x03}},
+		{0x08, []byte{0x00, 0x01, 0x02, 0x03}},
+		{0x0C, []byte{0x00, 0x01, 0x02, 0x03}},
+	}
+
+	testsExpected := []struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		{0x00, []byte{0x00, 0x01, 0x02, 0x03}, 4},
+		{0x04, []byte{0x00, 0x01, 0x02, 0x03}, 4},
+		{0x08, []byte{0x00, 0x01, 0x02, 0x03}, 4},
+		{0x0C, []byte{0x00, 0x01, 0x02, 0x03}, 4},
+	}
+
+	memory.Resize(16)
+
+	for _, test := range tests {
+		memory.Set4(test.offset, test.data)
+	}
+
+	for _, expected := range testsExpected {
+		data := memory.GetVal(expected.offset, expected.size)
+
+		if !bytes.Equal(expected.data, data) {
+			t.Error("Invalid memory value")
+		}
+	}
+}
+
+func TestMemory_Set4_dynamic(t *testing.T) {
+	memory := NewMemory()
+
+	tests := []struct {
+		offset uint32
+		data   []byte
+	}{
+		{0x00, []byte{0x00, 0x01, 0x02, 0x03}},
+		{0x04, []byte{0x00, 0x01, 0x02}},
+		{0x08, []byte{0x00, 0x01}},
+		{0x0C, []byte{0x00}},
+	}
+
+	testsExpected := []struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		{0x00, []byte{0x00, 0x01, 0x02, 0x03}, 4},
+		{0x04, []byte{0x00, 0x00, 0x01, 0x02}, 4},
+		{0x08, []byte{0x00, 0x00, 0x00, 0x01}, 4},
+		{0x0C, []byte{0x00, 0x00, 0x00, 0x00}, 4},
+	}
+
+	memory.Resize(16)
+
+	for _, test := range tests {
+		memory.Set4(test.offset, test.data)
+	}
+
+	for _, expected := range testsExpected {
+		data := memory.GetVal(expected.offset, expected.size)
+
+		if !bytes.Equal(expected.data, data) {
+			t.Error("Invalid memory value")
+		}
+	}
+}
+
+func TestMemory_Sets(t *testing.T) {
+	memory := NewMemory()
+
+	tests := []struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		{0x00, []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}, 8},
+		{0x08, []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06}, 7},
+		{0x0F, []byte{0x00, 0x01, 0x02, 0x03, 0x04}, 5},
+		{0x14, []byte{0x00, 0x01}, 2},
+	}
+
+	testsExpected := []struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		{0x00, []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07}, 8},
+		{0x08, []byte{0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06}, 7},
+		{0x0F, []byte{0x00, 0x01, 0x02, 0x03, 0x04}, 5},
+		{0x14, []byte{0x00, 0x01}, 2},
+	}
+
+	memory.Resize(32)
+
+	for _, test := range tests {
+		memory.Sets(test.offset, test.size, test.data)
+	}
+
+	for _, expected := range testsExpected {
+		data := memory.GetVal(expected.offset, expected.size)
+
+		if !bytes.Equal(expected.data, data) {
+			t.Error("Invalid memory value")
+		}
+	}
+}
+
+func TestMemory_GetPtr(t *testing.T) {
+	memory := NewMemory()
+
+	test := struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		0x00,
+		[]byte{0x00, 0x01, 0x02, 0x03},
+		4,
+	}
+
+	testModified := struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		0x00,
+		[]byte{0x04, 0x05, 0x06, 0x07},
+		4,
+	}
+
+	testExpected := struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		0x00,
+		[]byte{0x04, 0x05, 0x06, 0x07},
+		4,
+	}
+
+	memory.Resize(16)
+
+	memory.Set4(test.offset, test.data)
+
+	data := memory.GetPtr(test.offset, test.size)
+	if !bytes.Equal(test.data, data) {
+		t.Error("Invalid memory value")
+	}
+
+	memory.Sets(testModified.offset, testModified.size, testModified.data)
+	if !bytes.Equal(testExpected.data, data) {
+		t.Error("Invalid memory value")
+	}
+
+}
+
+func TestMemory_Resize(t *testing.T) {
+	memory := NewMemory()
+
+	memory.Resize(32)
+	if cap(memory.data) != 32 {
+		t.Error("Invalid memory size")
+	}
+
+	memory.Resize(64)
+	if cap(memory.data) != 64 {
+		t.Error("Invalid memory size")
+	}
+
+	// Can't resize less than before
+	memory.Resize(32)
+	if cap(memory.data) != 64 {
+		t.Error("Invalid memory size")
+	}
+}
+
+func TestMemory_Len(t *testing.T) {
+	memory := NewMemory()
+
+	test := struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		0x00,
+		[]byte{0x00, 0x01, 0x02, 0x03},
+		4,
+	}
+
+	memory.Resize(4)
+
+	memory.Set4(test.offset, test.data)
+
+	if memory.Len() != len(memory.data) {
+		t.Error("Invalid memory size")
+	}
+}
+
+func TestMemory_Data(t *testing.T) {
+	memory := NewMemory()
+
+	testBytes := []struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		{0x00, []byte{0x00, 0x01, 0x02, 0x03}, 4},
+		{0x04, []byte{0x04, 0x05, 0x06, 0x07}, 4},
+		{0x08, []byte{0x08, 0x09, 0x0A, 0x0B}, 4},
+		{0x0C, []byte{0x0C, 0x0D, 0x0E, 0x0F}, 4},
+	}
+
+	testExpected := struct {
+		offset uint32
+		data   []byte
+		size   uint32
+	}{
+		0x00,
+		[]byte{0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F},
+		16,
+	}
+
+	memory.Resize(16)
+
+	for _, test := range testBytes {
+		memory.Set4(test.offset, test.data)
+	}
+
+	if bytes.Equal(memory.Data(), testExpected.data) {
+		t.Error("Invalid memory data")
+	}
+}
