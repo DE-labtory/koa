53e3cf40266b4379d96bbcbc390aec0e8e8d6f1a
jun <leebduk@gmail.com>
2019-01-18 21:40:17 +0900

    test: add Encode operand test case

--- encoding/encode_test.go
+++ encoding/encode_test.go
@@ -16,9 +16,82 @@
 
 package encoding_test
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+
+	"github.com/DE-labtory/koa/encoding"
+)
 
 // TODO: implement test cases :-)
 func TestEncodeOperand(t *testing.T) {
+	tests := []struct {
+		operand      interface{}
+		expectedByte []byte
+		expectedErr  error
+	}{
+		{
+			operand:      true,
+			expectedByte: []byte{0x00, 0x00, 0x00, 0x01},
+			expectedErr:  nil,
+		},
+		{
+			operand:      false,
+			expectedByte: []byte{0x00, 0x00, 0x00, 0x00},
+			expectedErr:  nil,
+		},
+		{
+			operand:      "abc",
+			expectedByte: []byte{0x61, 0x62, 0x63},
+			expectedErr:  nil,
+		},
+		{
+			operand:      "1234567890",
+			expectedByte: []byte{0x31, 0x32, 0x33, 0x34, 0x35, 0x36, 0x37, 0x38, 0x39, 0x30},
+			expectedErr:  nil,
+		},
+		{
+			operand:      "~!@#$%^&*()_+",
+			expectedByte: []byte{0x7e, 0x21, 0x40, 0x23, 0x24, 0x25, 0x5e, 0x26, 0x2a, 0x28, 0x29, 0x5f, 0x2b},
+			expectedErr:  nil,
+		},
+		{
+			operand:      "12!@qw",
+			expectedByte: []byte{0x31, 0x32, 0x21, 0x40, 0x71, 0x77},
+			expectedErr:  nil,
+		},
+		{
+			operand:      1,
+			expectedByte: []byte{0x00, 0x00, 0x00, 0x01},
+			expectedErr:  nil,
+		},
+		{
+			operand:      23,
+			expectedByte: []byte{0x00, 0x00, 0x00, 0x17},
+			expectedErr:  nil,
+		},
+		{
+			operand:      456,
+			expectedByte: []byte{0x00, 0x00, 0x01, 0xc8},
+			expectedErr:  nil,
+		},
+		{
+			operand:      'c',
+			expectedByte: nil,
+			expectedErr:  encoding.EncodeError{'c'},
+		},
+	}
 
+	for i, test := range tests {
+		op := test.operand
+		byteCode, err := encoding.EncodeOperand(op)
+
+		if !bytes.Equal(byteCode, test.expectedByte) {
+			t.Fatalf("test[%d] - EncodeOperand() result wrong. expectedByte=%x, got=%x", i, test.expectedByte, byteCode)
+		}
+
+		if err != test.expectedErr {
+			t.Fatalf("test[%d] - EncodeOperand() result wrong. expectedByte=%x, got=%x", i, test.expectedByte, byteCode)
+		}
+	}
 }
