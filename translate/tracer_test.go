e8712a006e225a0909a073f60e2f9d267733e89e
KimJunHui <junhui820@gmail.com>
2019-02-04 17:43:51 +0900

    Implemented test cases for Define().

--- translate/tracer_test.go
+++ translate/tracer_test.go
@@ -16,14 +16,74 @@
 
 package translate_test
 
-import "testing"
+import (
+	"testing"
 
-// TODO: implement test cases :-)
+	"github.com/DE-labtory/koa/encoding"
+	"github.com/DE-labtory/koa/translate"
+)
+
 func TestMemoryTable_Define(t *testing.T) {
+	tests := []struct {
+		id           string
+		value        interface{}
+		expectedSize uint
+		err          error
+	}{
+		{
+			id:           "aInteger",
+			value:        int64(1),
+			expectedSize: 8,
+			err:          nil,
+		},
+		{
+			id:           "aBoolean",
+			value:        true,
+			expectedSize: 8,
+			err:          nil,
+		},
+		{
+			id:           "aString",
+			value:        "abc",
+			expectedSize: 3,
+			err:          nil,
+		},
+		{
+			id:           "aNotDefined",
+			value:        []byte{01, 02, 03},
+			expectedSize: 0,
+			err: encoding.EncodeError{
+				Operand: []byte{01, 02, 03},
+			},
+		},
+	}
 
-}
+	mTable := translate.NewMemoryTable()
 
-// TODO: implement test cases :-)
-func TestMemoryTable_Use(t *testing.T) {
+	for i, test := range tests {
+		prevOffset := mTable.MemoryCounter
 
+		entry, err := mTable.Define(test.id, test.value)
+
+		if err != nil && err.Error() != test.err.Error() {
+			t.Fatalf("test[%d] - Define() error wrong. expected=%v, err=%v", i, test.err, err)
+		}
+
+		if entry.Size != test.expectedSize {
+			t.Fatalf("test[%d] - Define() result wrong. expected=%d, got=%d", i, test.expectedSize, entry.Size)
+		}
+
+		if entry.Offset != prevOffset {
+			t.Fatalf("test[%d] - Define() result wrong. expected=%d, got=%d", i, prevOffset, entry.Offset)
+		}
+
+		if err == nil && mTable.EntryMap[test.id] != entry {
+			t.Fatalf("test[%d] - Define() result wrong. expected=%v, got=%v", i, mTable.EntryMap[test.id], entry)
+		}
+
+		expectedMemoryCounter := prevOffset + test.expectedSize
+		if mTable.MemoryCounter != expectedMemoryCounter {
+			t.Fatalf("test]%d] - Define() result wrong. expected=%d, got=%d", i, expectedMemoryCounter, mTable.MemoryCounter)
+		}
+	}
 }
