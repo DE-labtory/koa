a12f994c412f23fc6f1055a69856c045ea885648
KIMSJ <leesd556@gmail.com>
2019-02-06 22:14:03 +0900

    Implement Arguments.Pack() func

--- abi/argument.go
+++ abi/argument.go
@@ -16,14 +16,49 @@
 
 package abi
 
+import (
+	"encoding/json"
+	"strings"
+)
+
 type Argument struct {
 	Name string
-	Type string
+	Type Type
 }
 
 type Arguments []Argument
 
+type ArgumentMarshaling struct {
+	Name string
+	Type string
+}
+
+// UnmarshalJSON implements json.Unmarshaler interface
+func (argument *Argument) UnmarshalJSON(data []byte) error {
+	var arg ArgumentMarshaling
+	err := json.Unmarshal(data, &arg)
+	if err != nil {
+		return err
+	}
+	argument.Type, err = NewType(arg.Type)
+	if err != nil {
+		return err
+	}
+	argument.Name = arg.Name
+
+	return nil
+}
+
 // Pack returns series of Arguments type
-func (argument Arguments) Pack() string {
-	return ""
+// Example
+// function foo(int64 a, bool b)
+// Pack() extract "int64,bool"
+func (arguments Arguments) Pack() string {
+	var packedTypes []string
+
+	for _, argument := range arguments {
+		packedTypes = append(packedTypes, string(argument.Type.Type))
+	}
+
+	return strings.Join(packedTypes, ",")
 }
--- abi/argument_test.go
+++ abi/argument_test.go
@@ -14,4 +14,66 @@
  * limitations under the License.
  */
 
-package abi
+package abi_test
+
+import (
+	"testing"
+
+	"github.com/DE-labtory/koa/abi"
+)
+
+func TestArguments_Pack(t *testing.T) {
+	tests := []struct {
+		arguments    abi.Arguments
+		expectedPack string
+	}{
+		{
+			arguments: abi.Arguments{
+				abi.Argument{
+					Type: abi.Type{abi.String},
+				},
+				abi.Argument{
+					Type: abi.Type{abi.String},
+				},
+				abi.Argument{
+					Type: abi.Type{abi.String},
+				},
+			},
+			expectedPack: "string,string,string",
+		},
+		{
+			arguments: abi.Arguments{
+				abi.Argument{
+					Type: abi.Type{abi.String},
+				},
+				abi.Argument{
+					Type: abi.Type{abi.Boolean},
+				},
+				abi.Argument{
+					Type: abi.Type{abi.Integer64},
+				},
+			},
+			expectedPack: "string,bool,int64",
+		},
+		{
+			arguments: abi.Arguments{
+				abi.Argument{
+					Type: abi.Type{abi.Integer64},
+				},
+				abi.Argument{
+					Type: abi.Type{abi.Integer64},
+				},
+				abi.Argument{
+					Type: abi.Type{abi.Integer64},
+				},
+			},
+			expectedPack: "int64,int64,int64",
+		},
+	}
+
+	for _, test := range tests {
+		if test.arguments.Pack() != test.expectedPack {
+			t.Errorf("Invalid result off Arguments.Pack(). expected : %s, but got %s", test.arguments.Pack(), test.expectedPack)
+		}
+	}
+}
