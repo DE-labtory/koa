d6b3623b71bfee6a5de561e91ee3d3c8c4cf956d
zerofruit <14wnrkim@gmail.com>
2019-02-20 21:21:09 +0900

    fix: add returning ABI

--- koa.go
+++ koa.go
@@ -1,26 +1,32 @@
 package koa
 
 import (
+	"github.com/DE-labtory/koa/abi"
 	"github.com/DE-labtory/koa/parse"
 	"github.com/DE-labtory/koa/translate"
 	"github.com/DE-labtory/koa/vm"
 )
 
-func Compile(input string) (translate.Asm, error) {
+func Compile(input string) (translate.Asm, abi.ABI, error) {
 	ast, err := parse.Parse(
 		parse.NewTokenBuffer(
 			parse.NewLexer(input)))
 
 	if err != nil {
-		return translate.Asm{}, err
+		return translate.Asm{}, abi.ABI{}, err
 	}
 
 	asm, err := translate.CompileContract(*ast)
 	if err != nil {
-		return asm, err
+		return asm, abi.ABI{}, err
 	}
 
-	return asm, nil
+	a, err := translate.ExtractAbi(*ast)
+	if err != nil {
+		return asm, abi.ABI{}, err
+	}
+
+	return asm, *a, nil
 }
 
 func Execute(rawByteCode []byte, callFunc *vm.CallFunc) (int64, error) {
