8f84de20911abd7ef3e26237089ad5a39df40c09
boohyunsik <bhs9194@nate.com>
2019-02-27 00:40:33 +0900

    Implements skel functions for scope refactoring

--- symbol/scope.go
+++ symbol/scope.go
@@ -16,93 +16,95 @@
 
 package symbol
 
-import (
-	"bytes"
-	"fmt"
-	"sort"
-)
+import "github.com/DE-labtory/koa/ast"
 
-// NewScope() makes a new scope.
-func NewScope() *Scope {
-	s := make(map[string]Symbol)
-	return &Scope{
-		store: s,
-		inner: make([]*Scope, 0),
-		outer: nil,
-	}
-}
-
-// NewEnclosedScope makes a new scope which has outer scope.
-func NewEnclosedScope(outer *Scope) *Scope {
-	scope := NewScope()
-	scope.outer = outer
-	return scope
-}
-
-// Scope represent a variable's scope.
 type Scope struct {
 	store map[string]Symbol
 
-	inner []*Scope
-	outer *Scope
+	child  []*Scope
+	parent *Scope
 }
 
-// Getter return's variable's scope.
-// If a scope doesn't have a variable,
-// Program will search in outer scope.
-func (s *Scope) Get(name string) Symbol {
-	var scope = s
-	for scope != nil {
-		obj, ok := scope.store[name]
-		if ok {
-			return obj
-		}
-		scope = scope.outer
+func NewScope() *Scope {
+	return &Scope{
+		store:  make(map[string]Symbol),
+		parent: nil,
+	}
+}
+
+func NewEnclosedScope(p *Scope) *Scope {
+	s := &Scope{
+		store:  make(map[string]Symbol),
+		child:  make([]*Scope, 0),
+		parent: p,
+	}
+	p.child = append(p.child, s)
+	return s
+}
+
+func GenerateScope(c *ast.Contract) *Scope {
+	s := NewScope()
+	for _, f := range c.Functions {
+		ScopingFunctionParameter(f, s)
+		ScopingFunctionBody(f, s)
 	}
 	return nil
 }
 
-// Setter set a variable to target scope's map
-func (s *Scope) Set(name string, val Symbol) Symbol {
-	s.store[name] = val
-	return val
-}
-
-func (s *Scope) SetOuter(outer *Scope) {
-	s.outer = outer
-}
-
-func (s *Scope) GetOuter() *Scope {
-	return s.outer
-}
-
-func (s *Scope) AppendInner(in *Scope) {
-	s.inner = append(s.inner, in)
-}
-
-func (s *Scope) GetInner() []*Scope {
-	return s.inner
-}
-
-func (s *Scope) String() string {
-	var out bytes.Buffer
-	scope := s
-
-	for scope != nil {
-		// sort scope.store's keys alphabetically
-		keys := make([]string, 0)
-		for k := range scope.store {
-			keys = append(keys, k)
+func ScopingFunctionParameter(f *ast.FunctionLiteral, s *Scope) {
+	for _, p := range f.Parameters {
+		switch p.Type {
+		case ast.IntType:
+			{
+				s.store[p.Identifier.Name] = &Integer{Name: p.Identifier}
+			}
+		case ast.StringType:
+			{
+				s.store[p.Identifier.Name] = &String{Name: p.Identifier}
+			}
+		case ast.BoolType:
+			{
+				s.store[p.Identifier.Name] = &Boolean{Name: p.Identifier}
+			}
 		}
-		sort.Strings(keys)
-
-		out.WriteString("[ Scope ]\n")
-		for _, k := range keys {
-			out.WriteString(fmt.Sprintf("key:%s, symbol:%s\n",
-				k, scope.store[k].String()))
-		}
-
-		scope = scope.outer
 	}
-	return out.String()
+}
+
+func ScopingFunctionBody(b *ast.FunctionLiteral, s *Scope) {
+	for _, stmt := range b.Body.Statements {
+		switch implStmt := stmt.(type) {
+		case *ast.AssignStatement:
+			{
+
+			}
+		case *ast.ReturnStatement:
+			{
+
+			}
+		case *ast.BlockStatement:
+			{
+
+			}
+		case *ast.ExpressionStatement:
+			{
+
+			}
+		case *ast.ReassignStatement:
+			{
+
+			}
+		case *ast.IfStatement:
+			{
+
+			}
+		}
+	}
+}
+
+func ScopingExpression(exp *ast.Expression, scope *Scope) {
+
+}
+
+func ScopingIdentifier(ident *ast.Identifier, scope *Scope) {
+
 }
--- symbol/scope_test.go
+++ symbol/scope_test.go
@@ -17,193 +17,59 @@
 package symbol
 
 import (
-	"testing"
-
 	"github.com/DE-labtory/koa/ast"
+	"reflect"
+	"testing"
 )
 
-func TestNewEnclosedScope(t *testing.T) {
-	outer := NewScope()
-	s := NewEnclosedScope(outer)
-
-	if s.outer != outer {
-		t.Fatalf("testNewEnclosedScope() failed. outer must be set")
-	}
-
-	if len(s.store) > 0 {
-		t.Fatalf("testNewEnclosedScope() failed. store's size must be 0")
-	}
-}
-
 func TestNewScope(t *testing.T) {
-	s := NewScope()
-	if s.outer != nil {
-		t.Fatalf("testNewScope() failed. outer must be nil")
-	}
-
-	if len(s.store) > 0 {
-		t.Fatalf("testNewScope() failed. store's size must be 0")
-	}
-}
-
-func TestScopeGetter(t *testing.T) {
-	tests := []struct {
-		scope       Scope
-		want        string
-		expectedSym Symbol
-	}{
-		{
-			Scope{
-				store: map[string]Symbol{
-					"a": &Integer{&ast.Identifier{Name: "a"}},
-					"b": &Integer{&ast.Identifier{Name: "b"}},
-				},
-			},
-			"a",
-			&Integer{&ast.Identifier{Name: "a"}},
-		},
-		{
-			Scope{
-				store: map[string]Symbol{
-					"a": &Integer{&ast.Identifier{Name: "a"}},
-					"b": &Integer{&ast.Identifier{Name: "b"}},
-				},
-				outer: &Scope{
-					store: map[string]Symbol{
-						"c": &String{&ast.Identifier{Name: "c"}},
-					},
-				},
-			},
-			"c",
-			&String{&ast.Identifier{Name: "c"}},
-		},
-		{
-			Scope{
-				store: map[string]Symbol{
-					"a": &Integer{&ast.Identifier{Name: "a"}},
-					"b": &Integer{&ast.Identifier{Name: "b"}},
-				},
-			},
-			"c",
-			nil,
-		},
-	}
-
-	for i, test := range tests {
-		sym := test.scope.Get(test.want)
-		if sym != nil && test.expectedSym.String() != sym.String() {
-			t.Fatalf("test[%d] testScopeGetter() returns invalid symbol.\n"+
-				"expected=%s\n"+
-				"got=%s", i, test.expectedSym.String(), sym.String())
-		}
-
-		if sym != nil && test.expectedSym == nil {
-			t.Fatalf("test[%d] testScopeGetter() returns invalid symbol.\n"+
-				"expected=nil\n"+
-				"got=%s", i, sym.String())
-		}
-	}
-}
-
-func TestScopeSetter(t *testing.T) {
-	tests := []struct {
-		Scope  *Scope
-		Name   string
-		Symbol Symbol
-	}{
-		{
-			&Scope{
-				store: map[string]Symbol{},
-				outer: &Scope{},
-			},
-			"testInt",
-			&Integer{&ast.Identifier{Name: "testInt"}},
-		},
-		{
-			&Scope{
-				store: map[string]Symbol{},
-				outer: &Scope{},
-			},
-			"testBool",
-			&Boolean{&ast.Identifier{Name: "testBool"}},
-		},
-		{
-			&Scope{
-				store: map[string]Symbol{},
-				outer: &Scope{},
-			},
-			"testString",
-			&String{&ast.Identifier{Name: "testString"}},
-		},
-	}
-
-	for i, test := range tests {
-		symbol := test.Scope.Set(test.Name, test.Symbol)
-		if symbol.String() != test.Symbol.String() {
-			t.Fatalf("test[%d] - TestScopeSetter() wrong result.\n"+
-				"expected=%s\n"+
-				"got=%s", i, test.Symbol.String(), symbol.String())
-		}
-
-		if sym := test.Scope.Get(test.Name); sym.String() != test.Symbol.String() {
-			t.Fatalf("test[%d] - TestScopeSetter() must set in scope store", i)
-		}
-	}
 
 }
 
-func TestScopeString(t *testing.T) {
+func TestNewEnclosedScope(t *testing.T) {
+
+}
+
+func TestGenerateScope(t *testing.T) {
 	tests := []struct {
-		scope    Scope
-		expected string
+		contract ast.Contract
+		expected Scope
 	}{
 		{
-			Scope{
-				store: map[string]Symbol{
-					"a": &Integer{&ast.Identifier{Name: "a"}},
-					"b": &Integer{&ast.Identifier{Name: "b"}},
-				},
-			},
-			"[ Scope ]\nkey:a, symbol:a\nkey:b, symbol:b\n",
-		},
-		{
-			Scope{
-				store: map[string]Symbol{
-					"a": &Integer{&ast.Identifier{Name: "a"}},
-					"b": &Integer{&ast.Identifier{Name: "b"}},
-				},
-				outer: &Scope{
-					store: map[string]Symbol{
-						"c": &String{&ast.Identifier{Name: "c"}},
-					},
-					outer: nil,
-				},
-			},
-			"[ Scope ]\nkey:a, symbol:a\nkey:b, symbol:b\n[ Scope ]\nkey:c, symbol:c\n",
-		},
-		{
-			Scope{
-				store: map[string]Symbol{
-					"c": &Integer{&ast.Identifier{Name: "c"}},
-					"b": &Integer{&ast.Identifier{Name: "b"}},
-					"a": &Integer{&ast.Identifier{Name: "a"}},
-				},
-				outer: &Scope{
-					store: map[string]Symbol{
-						"d": &String{&ast.Identifier{Name: "d"}},
+			// contract which has one function named "foo", and it has one integer parameter named "a"
+			contract: ast.Contract{
+				Functions: []*ast.FunctionLiteral{
+					{
+						Name: &ast.Identifier{Name: "foo"},
+						Parameters: []*ast.ParameterLiteral{
+							{
+								Type:       ast.IntType,
+								Identifier: &ast.Identifier{Name: "a"},
+							},
+						},
 					},
 				},
 			},
-			"[ Scope ]\nkey:a, symbol:a\nkey:b, symbol:b\nkey:c, symbol:c\n[ Scope ]\nkey:d, symbol:d\n",
+			expected: Scope{
+				store: map[string]Symbol{
+					"foo": &Function{Name: "foo"},
+				},
+				parent: nil,
+				child: []*Scope{
+					{
+						store: map[string]Symbol{
+							"a": &Integer{Name: &ast.Identifier{Name: "a"}},
+						},
+					},
+				},
+			},
 		},
 	}
 
-	for i, test := range tests {
-		str := test.scope.String()
-		if str != test.expected {
-			t.Fatalf("test[%d] - TestScopeString() wrong result.\n"+
-				"expected :\n%s\n"+
-				"got :\n%s\n", i, test.expected, str)
+	for _, test := range tests {
+		scope := GenerateScope(&test.contract)
+		if !reflect.DeepEqual(test.expected, scope) {
+			t.Fatalf("")
 		}
 	}
 }
